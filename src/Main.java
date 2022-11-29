85995aebfb707753126b3efcf892b36d6b69c251
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2022-11-30 00:00:29 +0100

    Ajout d'un message de bienvenue au lancement de l'application

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launchGame();
+    }
+
+    public static void launchGame() {
+        setCharactersAvailable();
+        showGUI();
+    }
+
+    public static void setCharactersAvailable() {
+    }
+
+    public static void showGUI() {
+        System.out.println(
+                "█▄─█▀▀▀█─▄█▄─▄▄─█▄─▄███─▄▄▄─█─▄▄─█▄─▀█▀─▄█▄─▄▄─█\n██─█─█─█─███─▄█▀██─██▀█─███▀█─██─██─█▄█─███─▄█▀█\n▀▀▄▄▄▀▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀");
     }
 }
